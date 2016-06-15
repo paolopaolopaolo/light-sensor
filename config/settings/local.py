4b2d8ac3da895ad163b975686d5a3795bed8b632
Dean Mercado <dean@yeti.co>
2016-06-14 22:08:01 -0700

    Initial Commit: Setup basic structure

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "g_:wvwjfC9N:ErS#m=Y4W-*n>N:>nT_0ZCCFP0Ry"
+
+ALLOWED_HOSTS = ['*']
