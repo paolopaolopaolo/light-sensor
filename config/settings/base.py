7501370cb2abf56069cbde51188060497034b782
Dean Mercado <dean@yeti.co>
2016-08-16 22:51:08 -0700

    Make Email use TLS port 587 again

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 
 # Application definition
 
