a6e9f7299abe9b34ad969f48279110e781d93a9e
Dean Mercado <dean@yeti.co>
2016-08-17 19:47:30 -0700

    Tweak and fix everything with the `send_sensordata` task

--- sensor_data/management/commands/send_sensordata.py
+++ sensor_data/management/commands/send_sensordata.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import csv
 import datetime
 import zipfile
-import shutil
+import os
 
 class Command(BaseCommand):
     help = 'Clears sensor data (to speed up system)'
@@ -13,20 +13,19 @@ class Command(BaseCommand):
         parser.add_argument('email', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        to_email = options.get('email')
+        to_email = options.get('email')[1]
         now = datetime.datetime.now().isoformat()
-        total_sensor_data = SensorData.objects.all()
+        total_sensor_data = ([sensordata.timestamp, sensordata.light_level] for sensordata in SensorData.objects.iterator())
         with open('./lightsensor{}.csv'.format(now), 'w') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(['time', 'light_level'])
             for sensor_data in total_sensor_data:
-                csvwriter.writerow([sensor_data.timestamp, sensor_data.light_level])
-        with zipfile.Zipfile('./lightsensor{}.csv.zip'.format(now), 'w') as csvzip:
+                csvwriter.writerow(sensor_data)
+        with zipfile.ZipFile('./lightsensor{}.csv.zip'.format(now), 'w') as csvzip:
             csvzip.write('./lightsensor{}.csv'.format(now))
-            shutil.rmtree('./lightsensor{}.csv'.format(now))
+            os.remove('./lightsensor{}.csv'.format(now))
 
         from django.core.mail import EmailMessage
-
         email = EmailMessage(
             'Light Sensor: {}'.format(datetime.datetime.now().isoformat()),
             'Attached to this email is an amazing CSV. Enjoy!',
@@ -34,4 +33,6 @@ class Command(BaseCommand):
             [to_email, ],
         )
         email.attach_file('./lightsensor{}.csv.zip'.format(now))
+        os.remove('./lightsensor{}.csv.zip'.format(now))
         email.send()
+        SensorData.objects.all().delete()
