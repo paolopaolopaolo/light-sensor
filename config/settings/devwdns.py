460f6c91baab5f8881ac8397db713128b31cbeb9
Dean Mercado <dean@yeti.co>
2016-09-09 14:11:46 -0700

    Re-do the secrets for devwdns server

--- config/settings/devwdns.py
+++ config/settings/devwdns.py
@@ -1,7 +1,15 @@
 from .base import *
+import os
 
-DEBUG = True
+DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "{{ lookup('vault', 'secret/lightsensor_developmentwithdns', 'secret_key', 'vault_test.json') }}"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = ["light.dpmercado.com", ]
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
