2b736ef94469ae2319674ae0b310677f587d6f65
Dean Mercado <dean@yeti.co>
2017-05-05 14:39:03 -0700

    Make some animation fixes:
    * Reduce messed up frame time issues with animation, clean up display
    * Fix the animation blocking with more sophisticated technique

--- sensor_data/static/sensor_data/js/animations.js
+++ sensor_data/static/sensor_data/js/animations.js
@@ -1,143 +1,188 @@
-// Hex to RGB Conversions
 
-const _hexToRGBArray = (hexString) => {
-    const rgbArray = hexString
-        .match(/#{0,1}(\w{2})(\w{2})(\w{2})/)
-        .slice(1)
-        .map(val => parseInt(val, 16));
+// Boilerplate for NOT polluting that sweet, sweet global namespace.
+window.doEverything = (() => {
+    // Variables: Global
+    const width = 50;
+    const raf = window.requestAnimationFrame;
+    const stepSize = 0.01;
 
-    if (rgbArray.findIndex(val => val > 255 || val < 0) > -1) return; // returns null if hexString is invalid
-    return rgbArray;
-};
+    // Variables: DOM elements
 
-const _rgbArrayToHex = (rgbArray) => {
-    return `#${rgbArray.map((val) => {
-        const hexString = Math.floor(val).toString(16);
-        return hexString.length === 1 ? `0${hexString}` : hexString;
-    }).join('')}`;
-};
+    const background = document.getElementById('container');
+    const birdie = document.getElementsByClassName('birdie')[0];
+    const roof = document.getElementsByClassName('roof__front')[0];
+    const roof3D = document.getElementsByClassName('roof__3d')[0];
+    const houseBody = document.getElementsByClassName('house-body')[0];
+    const houseBody3D = document.getElementsByClassName('house-body-3d')[0];
 
-// Get style by progress
+    // Progress: Style Milestone mappings
 
-const getColorWithMilestones = (milestones, progress) => {
-    const integerProgress = Math.floor(progress * 100);
-    const milestoneKeys = Object.keys(milestones);
-    let startingFromIdx = milestoneKeys.findIndex(milestone => integerProgress < parseInt(milestone, 10)) - 1;
-    if (startingFromIdx < 0) startingFromIdx = milestoneKeys.length - 1;
-    const startingMilestone = milestoneKeys[startingFromIdx];
-    const endingMilestone = milestoneKeys[Math.min(startingFromIdx + 1, milestoneKeys.length - 1)];
-    const milestoneSize = ((parseInt(startingMilestone, 10) - parseInt(endingMilestone, 10))**2)**(1/2);
-    const fromArray = _hexToRGBArray(milestones[startingMilestone]);
-    const toArray = _hexToRGBArray(milestones[endingMilestone]);
-    const normalizedProgress = (integerProgress - startingMilestone) / milestoneSize;
-    // gets the difference between from and to on all rgb values
-    const diffArray = toArray.map((toItem, idx) => toItem - fromArray[idx]);
-    // gets the array of colors
-    const targetRGBArray = diffArray.map((diffItem, idx) => Math.max(fromArray[idx] + (diffItem * normalizedProgress), 0) );
-    const rgbString = _rgbArrayToHex(targetRGBArray);
-    return (rgbString);
-};
+    const dayPatternMilestones = {
+        '0': '#000000',
+        '50': '#003366',
+        '100': '#d5f3ff'
+    };
 
-const getXYByProgress = (width, percent) => {
-    const halfWidth = width * 1.85;
-    const heightCoeff = 0.9;
-    const x = (halfWidth) * percent;
-    const y = -((heightCoeff/halfWidth) * ((x - halfWidth)**2)) + (halfWidth * heightCoeff);
-    return [x, y];
-};
+    const sunMoonMilestones = {
+        '0': '#f3f5f6',
+        '25': '#FE7B65',
+        '100': '#ffff00'
+    };
 
-// Progress: Style Milestone mappings
+    const roofMilestones = {
+        '0': '#5a0303',
+        '100': '#ff0000'
+    };
 
-const dayPatternMilestones = {
-    '0': '#000000',
-    '50': '#003366',
-    '100': '#d5f3ff'
-};
+    const roofBackground = {
+        '0': '#500202',
+        '100': '#a00000'
+    };
 
-const sunMoonMilestones = {
-    '0': '#f3f5f6',
-    '25': '#FE7B65',
-    '100': '#ffff00'
-};
+    const houseBodyMilestones = {
+        '0': '#010133',
+        '100': '#1B52AA'
+    };
 
-const roofMilestones = {
-    '0': '#5a0303',
-    '100': '#ff0000'
-};
+    const houseBackground = {
+        '0': '#020232',
+        '100': '#25384b'
+    };
 
-const roofBackground = {
-    '0': '#500202',
-    '100': '#a00000'
-};
+    const windowMilestones = {
+        '0': '#ffff00',
+        '50': '#a6c5e4',
+        '100': '#bbbbbb'
+    };
 
-const houseBodyMilestones = {
-    '0': '#010133',
-    '100': '#1B52AA'
-};
+    // FUNCTIONS: Hex to RGB Conversions
+    const _hexToRGBArray = (hexString) => {
+        const rgbArray = hexString
+            .match(/#{0,1}(\w{2})(\w{2})(\w{2})/)
+            .slice(1)
+            .map(val => parseInt(val, 16));
 
-const houseBackground = {
-    '0': '#020232',
-    '100': '#25384b'
-};
+        if (rgbArray.findIndex(val => val > 255 || val < 0) > -1) return; // returns null if hexString is invalid
+        return rgbArray;
+    };
 
-const windowMilestones = {
-    '0': '#ffff00',
-    '50': '#a6c5e4',
-    '100': '#bbbbbb'
-};
+    const _rgbArrayToHex = (rgbArray) => {
+        return `#${rgbArray.map((val) => {
+            const hexString = Math.floor(val).toString(16);
+            return hexString.length === 1 ? `0${hexString}` : hexString;
+        }).join('')}`;
+    };
 
-const width = 50;
+    // FUNCTIONS: Get style by progress (0.0 - 1.0)
 
-// DOM elements
+    const getColorWithMilestones = (milestones, progress) => {
+        const integerProgress = Math.floor(progress * 100);
+        const milestoneKeys = Object.keys(milestones);
+        let startingFromIdx = milestoneKeys.findIndex(milestone => integerProgress < parseInt(milestone, 10)) - 1;
+        if (startingFromIdx < 0) startingFromIdx = milestoneKeys.length - 1;
+        const startingMilestone = milestoneKeys[startingFromIdx];
+        const endingMilestone = milestoneKeys[Math.min(startingFromIdx + 1, milestoneKeys.length - 1)];
+        const milestoneSize = Math.pow(Math.pow(parseInt(startingMilestone, 10) - parseInt(endingMilestone, 10), 2), 0.5);
+        const fromArray = _hexToRGBArray(milestones[startingMilestone]);
+        const toArray = _hexToRGBArray(milestones[endingMilestone]);
+        const normalizedProgress = (integerProgress - startingMilestone) / milestoneSize;
+        // gets the difference between from and to on all rgb values
+        const diffArray = toArray.map((toItem, idx) => toItem - fromArray[idx]);
+        // gets the array of colors
+        const targetRGBArray = diffArray.map((diffItem, idx) => Math.max(fromArray[idx] + (diffItem * normalizedProgress), 0) );
+        const rgbString = _rgbArrayToHex(targetRGBArray);
+        return (rgbString);
+    };
 
-const background = document.getElementById('container');
-const birdie = document.getElementsByClassName('birdie')[0];
-const roof = document.getElementsByClassName('roof__front')[0];
-const roof3D = document.getElementsByClassName('roof__3d')[0];
-const houseBody = document.getElementsByClassName('house-body')[0];
-const houseBody3D = document.getElementsByClassName('house-body-3d')[0];
+    const getYUpturntParabola = (width, percent) => {
+        const halfWidth = width * 1.85;
+        const heightCoeff = 0.9;
+        const x = (halfWidth) * percent;
+        const y = -((heightCoeff/halfWidth) * (Math.pow(x - halfWidth, 2))) + (halfWidth * heightCoeff);
+        return [x, y];
+    };
 
-// Animation frames
+    // FUNCTIONS: Animation frames (returning functions passed to rAF)
 
-const _colorStep = (el, milestones, pastProg, progLimit, asc = null, attribute = 'background-color') => () => {
-    if (asc === null) asc = pastProg - progLimit < 0;
-    const newProg = asc ? pastProg + 0.01 : pastProg - 0.01;
-    el.style[attribute] = getColorWithMilestones(milestones, newProg);
-    if (asc ? newProg < progLimit : newProg > progLimit) {
-        window.requestAnimationFrame(_colorStep(el, milestones, newProg, progLimit, asc, attribute));
-    }
-};
+    const _colorStep = (
+        el,
+        milestones,
+        pastProg,
+        progLimit,
+        asc,
+        attribute,
+        finishedAnimations
+    ) =>
+        () => {
+            if (asc === null) asc = pastProg - progLimit < 0;
+            const newProg = asc ? pastProg + stepSize : pastProg - stepSize;
+            el.style[attribute] = getColorWithMilestones(milestones, newProg);
+            if (asc ? newProg < progLimit : newProg > progLimit) {
+                raf(_colorStep(el, milestones, newProg, progLimit, asc, attribute, finishedAnimations));
+            } else {
+                finishedAnimations.push(true);
+            }
+        };
 
-const _parabolaStep = (el, width, pastProg, progLimit, asc = null) => () => {
-    console.log(`Start: ${performance.now()}`);
-    if (asc === null) asc = pastProg - progLimit < 0;
-    const newProg = asc ? pastProg + 0.01 : pastProg - 0.01;
-    const newXY = getXYByProgress(width, newProg);
-    el.style.left = `${newXY[0]}vw`;
-    el.style.bottom = `${newXY[1]}vh`;
-    if (asc ? newProg < progLimit : newProg > progLimit) {
-        window.requestAnimationFrame(_parabolaStep(el, width, newProg, progLimit, asc));
-    }
-    console.log(`End: ${performance.now()}`);
-};
+    const _parabolaStep = (
+        el,
+        width,
+        pastProg,
+        progLimit,
+        finishedAnimations,
+        asc = null
+    ) =>
+        () => {
+            if (asc === null) asc = pastProg - progLimit < 0;
+            const newProg = asc ? pastProg + stepSize : pastProg - stepSize;
+            const newXY = getYUpturntParabola(width, newProg);
+            el.style.left = `${newXY[0]}vw`;
+            el.style.bottom = `${newXY[1]}vh`;
+            if (asc ? newProg < progLimit : newProg > progLimit) {
+                raf(_parabolaStep(el, width, newProg, progLimit, finishedAnimations, asc));
+            } else {
+                finishedAnimations.push(true);
+            }
+        };
 
-// Entrypoint
+    // Entrypoint
+    return (from, to) => new Promise((resolve) => {
+        const wps = document.getElementsByClassName('wp');
+        const animationsToFinish = 23; // the number of individual animations performed below
+        let finishedAnimations = [];
 
-const doEverything = (from, to) => {
-    window.requestAnimationFrame(_colorStep(background, dayPatternMilestones, from, to));
-    window.requestAnimationFrame(_parabolaStep(birdie, width, from, to));
-    window.requestAnimationFrame(_colorStep(birdie, sunMoonMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(roof, roofMilestones, from, to, null, 'border-bottom-color'));
-    window.requestAnimationFrame(_colorStep(houseBody, houseBodyMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(roof3D, roofBackground, from, to));
-    window.requestAnimationFrame(_colorStep(houseBody3D, houseBackground, from, to));
-    const wps = document.getElementsByClassName('wp');
-    window.requestAnimationFrame(_colorStep(wps[0], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[1], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[2], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[3], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[4], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[5], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[6], windowMilestones, from, to));
-    window.requestAnimationFrame(_colorStep(wps[7], windowMilestones, from, to));
-};
+        raf(_colorStep(background, dayPatternMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_parabolaStep(birdie, width, from, to, finishedAnimations));
+        raf(_colorStep(birdie, sunMoonMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(roof, roofMilestones, from, to, null, 'border-bottom-color', finishedAnimations));
+        raf(_colorStep(houseBody, houseBodyMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(roof3D, roofBackground, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(houseBody3D, houseBackground, from, to, null, 'background-color', finishedAnimations));
+        // TODO: find a better way to do all these windows
+        raf(_colorStep(wps[0], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[1], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[2], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[3], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[4], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[5], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[6], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[7], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[8], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[9], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[10], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[11], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[12], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[13], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[14], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+        raf(_colorStep(wps[15], windowMilestones, from, to, null, 'background-color', finishedAnimations));
+
+        // Naïve, yet comprehensive way to ensure that animations are blocking.
+        // Don't resolve until all the animations have reached the end
+        const checker = setInterval(() => {
+            if (animationsToFinish === finishedAnimations.length) {
+                clearInterval(checker);
+                resolve();
+            }
+        }, 16);
+    });
+})();
--- sensor_data/static/sensor_data/js/index.js
+++ sensor_data/static/sensor_data/js/index.js
@@ -1,52 +1,63 @@
-var lastProgress = 0;
 
-function determineColor(data) {
-    var alpha;
-    if (data < 250) {
-        alpha = '1';
-    } else if (data < 500) {
-        alpha = '0.9';
-    } else if (data < 970) {
-        alpha = '0.7';
-    } else {
-        alpha = '0.3';
-    }
-    return 'rgba(72,165,226,' + alpha + ')';
-}
+(() => {
+    // Variables: Global
+    const doEverything = window.doEverything;
+    const progressArray = [0];
+    let inProgress = false;
+    let wsClient;
 
-function determineExtraMessage(data) {
-    var message;
-    if (data < 125) {
-        message = '- oh, so dark';
-    } else if (data > 980){
-        message = '- oof, it\'s like being on the sun';
-    }
-    return message;
-}
+    // Functions
+    const startClient = () => {
+        wsClient = new WebSocket('ws://light.dpmercado.com:8768/');
 
-function startClient () {
-    var wsClient = new WebSocket('ws://light.dpmercado.com:8768/');
+        wsClient.onopen = function () {
+            console.log('Connection open!');
+        };
 
-    wsClient.onopen = function () {
-        console.log('Connection open!');
+        wsClient.onmessage = function (message) {
+            const incomingData = parseInt(message.data, 10);
+            progressArray.push(incomingData / 1000);
+        };
+
+        wsClient.onerror = function (error) {
+            console.log(error);
+        };
+
+        wsClient.onclose = function () {
+            console.log('Connection Closed');
+            startClient();
+        };
     };
 
-    wsClient.onmessage = function (message) {
-        const incomingData = parseInt(message.data, 10);
-        const progress = incomingData / 1000;
-        doEverything(lastProgress, progress);
-        lastProgress = progress;
-    };
+    const firstTwoDiff = array => array[0] !== array[1];
 
-    wsClient.onerror = function (error) {
-        console.log(error);
-    };
+    // Start Websocket
+    startClient();
 
-    wsClient.onclose = function () {
-        console.log('Connection Closed');
-        startClient();
-    };
-}
+    // Set interval for animation
+    setInterval(() => {
+        if (progressArray.length > 1) {
+            if (firstTwoDiff(progressArray)) {
+                if (!inProgress) {
+                    inProgress = true;
+                    let from, to;
+                    if (progressArray.length > 5) {
+                        from = progressArray.splice(0, 4)[0];
+                        to = progressArray[0];
+                    } else {
+                        from = progressArray.shift();
+                        to = progressArray[0];
+                    }
+                    doEverything(from, to).then(() => {
+                        inProgress = false;
+                    });
+                }
+            } else {
+                progressArray.shift();
+            }
+        }
+    }, 16);
 
-startClient();
+})();
+
 
